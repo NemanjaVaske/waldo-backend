8d534d1fa2a3da109dfd08fbe543f6f2769957c2
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-03-10 19:08:57 +0100

    add item to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+firstImage = Image.create(name: "easy", url: "easy.jpg");
+
+firstImage.characters.create(name: 'waldo', x_pos: 856, y_pos: 439)
+firstImage.characters.create(name: 'wenda', x_pos: 491, y_pos: 252)
+firstImage.characters.create(name: 'wizard', x_pos: 70, y_pos: 455)
+firstImage.characters.create(name: 'odlaw', x_pos: 318, y_pos: 389)
+
+secondImage = Image.create(name: "easy", url: "easy.jpg");
+secondImage
+secondImage.characters.create(name: 'waldo', x_pos: 406, y_pos: 376)
+secondImage.characters.create(name: 'wenda', x_pos: 295, y_pos: 319)
+secondImage.characters.create(name: 'wizard', x_pos: 781, y_pos: 349)
+secondImage.characters.create(name: 'odlaw', x_pos: 71, y_pos: 415)
